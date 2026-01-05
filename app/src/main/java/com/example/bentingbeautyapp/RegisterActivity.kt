05950b8756392d3dae94cc0b8324180189085928
Opjasson <azmitheusos333@gmail.com>
2026-01-05 15:07:54 +0700

    RegisterActivity : done setting listener go to login page

--- app/src/main/java/com/example/bentingbeautyapp/RegisterActivity.kt
+++ app/src/main/java/com/example/bentingbeautyapp/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bentingbeautyapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,10 +16,17 @@ class RegisterActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.apply {
+            RGotoLogin.setOnClickListener {
+                startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+            }
         }
     }
 }
