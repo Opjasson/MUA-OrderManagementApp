4bcff166c1842d7d965bcb4b4b0a97fa6657d5aa
Opjasson <azmitheusos333@gmail.com>
2026-01-05 22:18:49 +0700

    ChangePasswordLayout : done setting layout page

--- app/src/main/java/com/example/bentingbeautyapp/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/bentingbeautyapp/ChangePasswordActivity.kt
@@ -1,36 +1,20 @@
 package com.example.bentingbeautyapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.bentingbeautyapp.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
-
+class ChangePasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_change_password)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.apply {
-            LGotoRegister.setOnClickListener {
-                startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
-            }
-
-            forgotPassTxt.setOnClickListener {
-                startActivity(Intent(this@LoginActivity, ForgotPasswordActivity::class.java))
-            }
         }
     }
 }
