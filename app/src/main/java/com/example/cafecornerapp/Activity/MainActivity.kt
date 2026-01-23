f3b0d035c2dcfb576846ae39659b7129303bf489
Opjasson <azmitheusos333@gmail.com>
2026-01-23 17:49:40 +0700

    layoutXML : add drawer header

--- app/src/main/java/com/example/cafecornerapp/Activity/MainActivity.kt
+++ app/src/main/java/com/example/cafecornerapp/Activity/MainActivity.kt
@@ -5,17 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.cafecornerapp.R
+import com.example.cafecornerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+        drawerLayout = binding.drawerLayout
     }
 }
