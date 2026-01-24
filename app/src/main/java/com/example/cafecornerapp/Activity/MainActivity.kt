4d48f8b56f846fd78c7f44ceb6f6f743ccb9078b
Opjasson <azmitheusos333@gmail.com>
2026-01-24 07:15:07 +0700

    drawable : add icon menu

--- app/src/main/java/com/example/cafecornerapp/Activity/MainActivity.kt
+++ app/src/main/java/com/example/cafecornerapp/Activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.cafecornerapp.Activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -25,9 +27,42 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+       initSideBar()
+    }
+
+    private fun initSideBar () {
         val toolbar = binding.toolbar
         setSupportActionBar(toolbar)
 
         drawerLayout = binding.drawerLayout
+
+        val navigationView = binding.navigationView
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open,
+            R.string.close
+        )
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+//                R.id.menu_profile -> {
+//                    // buka profile
+//                }
+//                R.id.menu_logout -> {
+////                    logout()
+//                }
+            }
+            drawerLayout.closeDrawers()
+            true
+        }
     }
 }
